9320c137b90d1f158a9e29b438fdc5d53860e4df
Mikhail Bortnyk <vessimir@gmail.com>
2012-10-13 23:01:01 +0300

    Reworked path_prefix for omniauth

--- config/initializers/omniauth_github.rb
+++ config/initializers/omniauth_github.rb
@@ -1,5 +1,7 @@
-OmniAuth.config.path_prefix = 'settings/auth'
-
 Rails.application.middleware.use OmniAuth::Builder do
+  configure do |config|
+    config.path_prefix = '/settings/auth'
+  end
+
   provider :github, Rails.application.config.github_app_id, Rails.application.config.github_app_secret
 end
