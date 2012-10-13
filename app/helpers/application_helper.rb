39b4683fd6dccdb0fb03070f372077da5792d651
miroshko <miroshko@gmail.com>
2012-10-13 20:20:38 +0300

    flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def flash_class(level)
+    case level
+      when :notice then "info"
+      when :error then "error"
+      when :alert then "warning"
+    end
+  end
 end
