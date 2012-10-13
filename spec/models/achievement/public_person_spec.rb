2a870a659ac957c9f824b88db02ddde359c8bb46
Alex Rozumey <brain-geek@yandex.ua>
2012-10-13 15:37:11 +0300

    Add empty public person spec, refactor tests

--- spec/models/achievement/public_person_spec.rb
+++ spec/models/achievement/public_person_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Achievement::ForeverAlone do
+describe Achievement::PublicPerson do
   it "should be included in Achievement list" do
     Achievement.list.should include described_class
   end
@@ -9,12 +9,12 @@ describe Achievement::ForeverAlone do
     let(:user) { Fabricate :user}
 
     it "should call github and check there if positive" do
-      Github.should_receive(:forever_alone?).and_return false
+      Github.should_receive(:public_person?).and_return false
       described_class.check(user).should be_false
     end
 
     it "should call github and check there if negative" do
-      Github.should_receive(:forever_alone?).and_return true
+      Github.should_receive(:public_person?).and_return true
       described_class.check(user).should be_true
     end
   end
