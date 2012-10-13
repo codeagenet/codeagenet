e540cf7f37c8fc1c93d72921c8ab0ae7d16ce6b9
miroshko <miroshko@gmail.com>
2012-10-13 16:37:57 +0300

    noemail page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Codeage::Application.routes.draw do
   devise_for :users
   match '/auth/:provider/callback' => 'authentications#create'
+  match '/about' => 'main#about'
+  match '/noemail' => 'main#noemail'
   root :to => 'main#index'
 end
