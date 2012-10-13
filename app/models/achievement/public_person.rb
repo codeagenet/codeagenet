d0697b06e4e75a2dd2af9c9cfd55d8a139d319cc
Mikhail Bortnyk <vessimir@gmail.com>
2012-10-13 17:18:19 +0300

    Added public person achievement realization

--- app/models/achievement/public_person.rb
+++ app/models/achievement/public_person.rb
@@ -1,5 +1,7 @@
 class Achievement::PublicPerson < Achievement
   def self.check(user)
-    Github.public_person?
+    followers = user.github.users.followers.all.map { |f| f.login }
+    following = user.github.users.followers.following.map { |f| f.login }
+    (followers - following).count > 10
   end
 end
--- spec/models/achievement/public_person_spec.rb
+++ spec/models/achievement/public_person_spec.rb
@@ -2,17 +2,4 @@ require 'spec_helper'
 
 describe Achievement::PublicPerson do
   it_should_behave_like "achievement"
-  let(:user) { Fabricate :user}
-
-  describe "check" do
-    it "should call github and check there if positive" do
-      Github.should_receive(:public_person?).and_return false
-      described_class.check(user).should be_false
-    end
-
-    it "should call github and check there if negative" do
-      Github.should_receive(:public_person?).and_return true
-      described_class.check(user).should be_true
-    end
-  end
 end
