e7a626fb3254cf5b8e6cc9616306a89b94cee869
miroshko <miroshko@gmail.com>
2012-10-13 18:38:22 +0300

    profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Codeage::Application.routes.draw do
+  get "user/profile"
+
   devise_for :users
   match '/auth/:provider/callback' => 'authentications#create'
   match '/about' => 'main#about'
   match '/noemail' => 'main#noemail'
-  match '/profile' => 'main#profile'
+  match '/profile' => 'user#profile'
   root :to => 'main#index'
 end
