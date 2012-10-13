48b097fe2d66d84de1ada1259b42c14b33fabea3
Mikhail Bortnyk <vessimir@gmail.com>
2012-10-13 19:09:05 +0300

    Fixes for noemail again

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -5,17 +5,22 @@ class AuthenticationsController < ApplicationController
 
     if auth['info']['email'].blank?
       redirect_to noemail_url
+    else
+      authentication = Authentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
+      authenticate! authentication
     end
+  end
 
-    authentication = Authentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
+  private
 
+  def authenticate! authentication
     if authentication
       flash[:notice] = "Signed in successfully"
       sign_in_and_redirect(:user, authentication.user)
     else
       user = User.new
       user.apply_omniauth(auth)
-      if user.save(validate: false)
+      if user.save
         flash[:notice] = "Account created and signed in successfully"
         sign_in_and_redirect(:user, user)
       else
