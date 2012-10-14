b529b2f472c661dae42790d84efe288f4580a76e
Alex Rozumey <brain-geek@yandex.ua>
2012-10-14 19:07:13 +0300

    Fix 500?

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,9 +2,6 @@ class UserController < ApplicationController
   protect_from_forgery
   before_filter :authenticate_user!, :only => [:profile, :force_earn]
 
-  def profile
-  end
-
   def public
     username = request.params[:username]
     @user = User.find_by_nickname! username
