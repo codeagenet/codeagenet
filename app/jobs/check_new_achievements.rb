d7922e3fee9a90e9ef22de17a7a752f9dac14af9
Alex Rozumey <brain-geek@yandex.ua>
2012-10-23 18:23:07 +0300

    Fix race condition bug, when background and realtime workers tried to check achievements for same user at once

--- app/jobs/check_new_achievements.rb
+++ app/jobs/check_new_achievements.rb
@@ -2,6 +2,6 @@ class CheckNewAchievements
   @queue = :cron
 
   def self.perform
-    User.all.each{|u| u.earn_achievements }
+    User.where(User.arel_table[:achievements_fetched_at].not_eq(nil)).each{|u| u.earn_achievements }
   end
 end
